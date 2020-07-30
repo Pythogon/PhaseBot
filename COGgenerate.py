c4e0ee23fff3c1a23b2c54d63c504af0dc9a4231
iCielin <sky@sirlovespikachu.uk>
2020-07-30 19:59:24 +0100

    TODO implementation framework

---
+++ COGgenerate.py
@@ -0,0 +1,12 @@
+import discord
+import glo
+
+from discord import commands
+
+class Generate(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    """
+    TODO Generate cog implementation
+    """
