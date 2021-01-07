d2af71e7d8f13d55bba585597b0837c01530682e
Dec <31135263+iCielin@users.noreply.github.com>
2021-01-07 00:02:37 +0000

    Let's test

--- root/PhaseBot/Cogs/Generators.py
+++ root/PhaseBot/Cogs/Generators.py
@@ -13,7 +13,7 @@ class Generators(commands.Cog):
 
     @commands.command(aliases = ["star"])
     async def startotal(self, ctx):
-        read = len(glo.FILEREAD("starcastle.txt"))
+        read = len(glo.FILEREAD("starcastle.txt")) + 55610
         percent = float('{:g}'.format(float('{:.{p}g}'.format((read / 100000) * 100, p=4)))) # Number to 4 s.f.
         started = date(2020, 9, 30) # Unchanging start date of development and collection of starboard entries
         today = date.today() 
