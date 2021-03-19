79c7d319e23ee6305b6bb5fdfa7cab395726beff
jsuis <ash.thawl@gmail.com>
2021-03-19 10:52:08 +0000

    Why oh why

--- root/PhaseBot/Cogs/Currency.py
+++ root/PhaseBot/Cogs/Currency.py
@@ -118,7 +118,7 @@ class Bank(commands.Cog):
         msg: discord.Message = await ctx.send(embed = ebd)
         for r in current_items:
             if r in custom_emoji_map:
-                await msg.add_reaction(f"{await ctx.guild.fetch_emoji(int(custom_emoji_map[r]))}")
+                await msg.add_reaction(await ctx.guild.fetch_emoji(int(custom_emoji_map[r])))
             else:
                 await msg.add_reaction(r)
 
