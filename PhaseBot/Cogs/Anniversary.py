37249e55bb16ef1ee9adbfeb267b9a7d2f7070f1
jsuis <76794094+jsuis@users.noreply.github.com>
2021-04-07 15:16:51 +0100

    3.1

---
+++ PhaseBot/Cogs/Anniversary.py
@@ -0,0 +1,11 @@
+import discord
+import glo 
+
+from discord.ext import commands
+
+class Anniversary(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    # TODO: ANNIVERSARYCODE
