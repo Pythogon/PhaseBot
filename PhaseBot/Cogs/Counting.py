8b44f25de3dcd9b04d6aeef6e8a424d2cc63db82
jsuis <76794094+jsuis@users.noreply.github.com>
2021-04-12 18:43:30 +0100

    Merge counting storage into 1 file (#14)

--- PhaseBot/Cogs/Counting.py
+++ PhaseBot/Cogs/Counting.py
@@ -11,20 +11,21 @@ class Counting(commands.Cog):
     @commands.command()
     async def revive(self, ctx, *message):
         counting_channel = ctx.guild.get_channel(glo.COUNTING_CHANNEL)
+        data = glo.JSONREAD("counting.json")
         message = " ".join(message)
-        record = glo.FILEREAD("counting_record.txt")
         user = glo.USERDATA_READ(ctx.author.id)
         if user["currency"] < glo.REVIVE_COST: return await ctx.send("You don't have enough money to do that!")
         user["currency"] -= 1000
-        glo.FILEWRITE("counting_lastnumber.txt", record)
+        data["number"] = data["record"]
+        glo.JSONWRITE("counting.json", data)
         glo.USERDATA_WRITE(ctx.author.id, user)
-        embed = discord.Embed(title = f"Revive activated by {ctx.author.name}!", description = f"Current count: {record}.", color = 0x00ff00) \
+        embed = discord.Embed(title = f"Revive activated by {ctx.author.name}!", description = f"Current count: {data['record']}.", color = 0x00ff00) \
         .set_thumbnail(url=ctx.author.avatar_url) \
         .set_footer(text=glo.FOOTER())
         if message != "":
             embed.add_field(name = "Revive message", value = message)
         await counting_channel.send(embed = embed)
-        await ctx.send("Revived! Thank you!")
+        await ctx.send(f"Revived! You have been debited {glo.REVIVE_COST} {glo.BANKFORMAT(glo.REVIVE_COST)}.")
     
     @commands.Cog.listener(name = "on_message") 
     async def counting_handler(self, message):
@@ -35,14 +36,11 @@ class Counting(commands.Cog):
         if message.channel.id != glo.COUNTING_CHANNEL: return      
 
         counting_channel = message.channel
-        counting_lastnumber = "counting_lastnumber.txt"
-        counting_lastuser = "counting_lastuser.txt"   
-        err = 0
-        last_user = glo.FILEREAD(counting_lastuser)
-        glo.FILEWRITE(counting_lastuser, str(message.author.id)) 
-        n = int(glo.FILEREAD(counting_lastnumber))
+        data = glo.JSONREAD("counting.json")
+        err = 0 
+        n = data["number"]
         nn = n + 1
-        record = int(glo.FILEREAD("counting_record.txt"))
+        record = data["record"]
 
         await message.delete()
         embed = discord.Embed(title="New count", description=message.content, color = glo.COLOR) \
@@ -52,13 +50,15 @@ class Counting(commands.Cog):
         await counting_channel.send(embed = embed)  
         
         if message.content.startswith(str(nn)) != True: err = 1
-        if str(message.author.id) == last_user: err = 2
+        if message.author.id == data["user"]: err = 2
 
         if err == 0: 
+            data["number"] = nn
+            data["user"] = message.author.id
             ud = glo.USERDATA_READ(message.author.id)
             ud["currency"] += 1
             glo.USERDATA_WRITE(message.author.id, ud)
-            return glo.FILEWRITE(counting_lastnumber, str(nn))
+            return glo.JSONWRITE("counting.json", data)
         
         reason = {1: "That wasn't the correct next number!", 2: "You can't send 2 numbers in a row!"}.get(err)
 
@@ -66,15 +66,16 @@ class Counting(commands.Cog):
         .add_field(name = "Fail reason", value = reason, inline = False) \
         .set_thumbnail(url = message.author.avatar_url) \
         .set_footer(text = glo.FOOTER())
-
-        glo.FILEWRITE(counting_lastuser, "0")
-        glo.FILEWRITE(counting_lastnumber, "0")
+        
+        data["number"] = 0
+        data["user"] = 0
 
         if n > record:
             diff = n - record
             embed.add_field(name = "That was a new record!", value = f"By a difference of {diff}, this beat {record}.", inline = False)
             await counting_channel.edit(topic = f"Last Record: {n}")
-            glo.FILEWRITE("counting_record.txt", str(n))
+            data["record"] = n
 
+        glo.JSONWRITE("counting.json", data)
         await counting_channel.send(embed = embed)
     
