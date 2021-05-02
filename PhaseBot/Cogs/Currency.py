30969e10495b1b2f0f9b50e831e6898ea13ad600
jsuis <76794094+jsuis@users.noreply.github.com>
2021-05-02 17:16:15 +0100

    nn

--- PhaseBot/Cogs/Currency.py
+++ PhaseBot/Cogs/Currency.py
@@ -251,5 +251,5 @@ class Bank(commands.Cog):
         to_send = "**Current tax brackets**\n\n"
         for k, v in glo.TAX_BRACKETS.items():
             to_send += f"{k} {glo.BANKFORMAT(k)}: {100-v}%\n"
-        to_send += "\nKey:\n ```Max balance permitted for this bracket: Tax rate```"
+        to_send += "*Key: Max balance permitted for this bracket: Tax rate*"
         await ctx.send(to_send)
