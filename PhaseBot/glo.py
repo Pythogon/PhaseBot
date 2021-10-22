f87fd22428e6bb09b1428403eef9512bd7dda851
jsuis <76794094+jsuis@users.noreply.github.com>
2021-10-22 17:01:41 +0100

    v3.2.1

--- PhaseBot/glo.py
+++ PhaseBot/glo.py
@@ -68,7 +68,7 @@ TAX_BRACKETS = {-1: 100,
 3500: 5,
 9999: 0}
 TEMP_MESSAGE_LIST = []
-VERSION = "3.2.0.2" # Current version (entirely symbolic, means nothing)
+VERSION = "3.2.1" # Current version (entirely symbolic, means nothing)
 
 def BANKFORMAT(num):
     if num == 1: out = f"{num} <:bean:710243429119950969>"
