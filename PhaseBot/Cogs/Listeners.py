9a0ed81de7fd2a79c25cd950e8fa073f9e844df0
Asheiou <declanthawley@gmail.com>
2022-02-09 11:16:57 +0000

    Support multiword calls

--- PhaseBot/Cogs/Listeners.py
+++ PhaseBot/Cogs/Listeners.py
@@ -64,7 +64,7 @@ class Listeners(commands.Cog):
         calls = glo.GLOBAL_READ("calls")
         message_words = [x.lower() for x in message.content.split(" ")]
         for call, response in calls.items():
-            if call.lower() in message_words:
+            if [x.lower() for x in call.split(" ")] in message_words:
                 await message.channel.send(response)
 
     @commands.Cog.listener()
