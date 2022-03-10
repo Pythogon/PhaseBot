2113e3b7adaa17b654688c590b55bd8c82b6f97b
Asheiou <declanthawley@gmail.com>
2022-03-10 16:37:07 +0000

    Don't async __init__

--- PhaseBot/Cogs/Voting.py
+++ PhaseBot/Cogs/Voting.py
@@ -5,7 +5,7 @@ import instascrape
 from discord.ext import commands
 
 class Voting(commands.Cog):
-	async def __init__(self, bot):
+	def __init__(self, bot):
 		self.bot = bot
 		self._last_member = None
 
