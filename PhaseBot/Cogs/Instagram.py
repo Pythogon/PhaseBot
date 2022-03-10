c2efde3aaef829cebb60dcd572dbea4400ed4def
Asheiou <declanthawley@gmail.com>
2022-03-10 21:03:14 +0000

    Add a check new posts-er

--- PhaseBot/Cogs/Instagram.py
+++ PhaseBot/Cogs/Instagram.py
@@ -9,19 +9,29 @@ from datetime import datetime
 from discord.ext import commands
 from discord.ext import tasks
 
-class Voting(commands.Cog):
+class Instagram(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self._last_member = None
 		self.reload.start()
+		self.check_new_posts.start()
 
 	def cog_unload(self):
 		self.reload.cancel()
+		self.check_new_posts.cancel()
 
 	@tasks.loop(minutes = 5)
 	async def reload(self):
 		subprocess.Popen(shlex.split(f"instagram-scraper sole_nyu -m 1 --comments --media-types=none -u life_vote_counter -p{glo.GLOBAL_READ('igpass')} -d ./local_Store"))
 		glo.GLOBAL_WRITE('lastreload', round(datetime.now().timestamp()))
+
+	@tasks.loop(minutes = 10)
+	async def check_new_posts(self):
+		c = self.bot.get_channel(709719731472564224)
+		shortcode = glo.JSONREAD("sole_nyu.json")[0]["shortcode"]
+		if shortcode != glo.GLOBAL_READ("lastshortcode"): 
+			glo.GLOBAL_WRITE("lastshortcode", shortcode)
+			await c.send(f"A new episode of LIFE has been released! https://instagram.com/p/{shortcode}/")
 
 	@commands.command()
 	async def votes(self, ctx, number: int):
--- PhaseBot/Cogs/__init__.py
+++ PhaseBot/Cogs/__init__.py
@@ -2,7 +2,7 @@ from .Admin import Admin
 from .Counting import Counting
 from .Currency import Bank
 from .General import General
+from .Instagram import Instagram
 from .Listeners import Listeners
 from .Starboard import Starboard
 from .Tasks import Tasks
-from .Voting import Voting
